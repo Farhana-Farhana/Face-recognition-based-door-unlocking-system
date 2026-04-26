3def5cca947b14bea4fd2328c3160d087e09b8b0
N S Farhana <nsfarhana.2004@gmail.com>
2026-04-26 22:08:47 +0530

    Update face_recog.py

--- face_recog.py
+++ face_recog.py
@@ -1,6 +1,9 @@
 import face_recognition
 import cv2
 import pickle
+import requests
+
+ESP32_IP = "http://192.168.1.100/unlock" 
 
 ENCODINGS_FILE = "encodings.pickle"
 
@@ -8,6 +11,8 @@ with open(ENCODINGS_FILE, "rb") as f:
     data = pickle.load(f)
 
 video_capture = cv2.VideoCapture(0)
+
+last_unlocked = False  # prevent multiple requests
 
 while True:
     ret, frame = video_capture.read()
@@ -25,6 +30,18 @@ while True:
             name = data["names"][matchedIdx]
 
         print("Detected:", name)
+
+        # 🔓 Unlock if recognized
+        if name != "Unknown" and not last_unlocked:
+            try:
+                requests.get(ESP32_IP)
+                print("Door Unlock Triggered!")
+                last_unlocked = True
+            except:
+                print("ESP32 not reachable")
+
+        if name == "Unknown":
+            last_unlocked = False
 
     cv2.imshow("Face Recognition", frame)
 
